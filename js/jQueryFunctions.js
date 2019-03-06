b03c82543b29b3de739f1d2bc175e5a7fbbfe709
ChrisH_ <hobdy.chris02@gmail.com>
2019-03-06 10:19:37 -0500

    added header img

--- js/jQueryFunctions.js
+++ js/jQueryFunctions.js
@@ -1,12 +1,4 @@
 $(function() {
-  var hasBeenTrigged = false;
-  $(window).scroll(function() {
-    if ($(this).scrollTop() >= 100 && !hasBeenTrigged) {
-      // if scroll is greater/equal then 100 and hasBeenTrigged is set to false.
-      alert("You've scrolled 100 pixels.");
-      hasBeenTrigged = true;
-    }
-  });
   window.onscroll = () => {
     var scroll = $(window).scrollTop();
     var nav = $(".navbar");
@@ -17,4 +9,9 @@ $(function() {
       nav.removeClass("nav-finish").addClass("nav-start");
     }
   };
+
+  var headerImg = $("#mastTmg");
+  headerImg.bind("load", function() {
+    $(this).fadeIn("slow");
+  });
 });
